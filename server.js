a301bbf9f1ab3f040170f53bbbf89183b3a9bb8a
Tom Chestnut <tchestnut85@gmail.com>
2022-06-27 21:14:39 -0400

    added swagger-ui documentation

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const routes = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+app.use('/docs', swaggerUi.serveFiles(swaggerDoc), swaggerUi.setup(swaggerDoc));
+
 app.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}`);
+	console.log(`Server started on port: ${PORT}`);
 });
