afd32e773b1e88bafd75ea1f90699311c8d30163
Tom Chestnut <tchestnut85@gmail.com>
2022-06-30 20:30:44 -0400

    finished review endpoints for swagger-v2 docs

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const routes = require('./controllers');
 const swaggerUi = require('swagger-ui-express');
 
+const swaggerDocV1 = require('./swagger-v1.json');
+const swaggerDocV2 = require('./swagger-v2.json');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const swaggerDoc = require('./swagger.json');
 const swaggerOptions = {
-	// defaultModelsExpandDepth: -1, // uncomment to remove schemas from docs
+	// defaultModelsExpandDepth: -1,  // uncomment to remove schemas from docs
 };
 
 app.use(express.json());
@@ -15,9 +17,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 app.use(
-	'/docs',
-	swaggerUi.serveFiles(swaggerDoc, { swaggerOptions }),
-	swaggerUi.setup(swaggerDoc)
+	'/docs/v1',
+	swaggerUi.serveFiles(swaggerDocV1, { swaggerOptions }),
+	swaggerUi.setup(swaggerDocV1)
+);
+
+app.use(
+	'/docs/v2',
+	swaggerUi.serveFiles(swaggerDocV2, { swaggerOptions }),
+	swaggerUi.setup(swaggerDocV2)
 );
 
 app.listen(PORT, () => {
